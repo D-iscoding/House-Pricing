f76b512f56a9c63a04c7cdc6d2603fb9c86f6336
Daud Abdullahi <Daudabdullahi36@gmail.com>
2025-04-23 22:46:48 -0700

    p5 done

--- Houses.py
+++ Houses.py
@@ -1,42 +1,38 @@
-class House:
-    def __init__(self, year_built, price_purchased, location):
-        self.year_built = year_built
-        self.price_purchased = price_purchased
-        self.location = location
-        self.current_value = 0.0
+def calculate_current_value(year_built, price_purchased, current_year):
+    rate = 0.08
+    age = current_year - year_built
+    return price_purchased * ((1 + rate) * age)
 
-    def calculate_current_value(self, current_year):
-        rate = 0.08
-        age = current_year - self.year_built
-        self.current_value = self.price_purchased * ((1 + rate) ** age)
+def money_earned(current_value, price_purchased):
+    return current_value - price_purchased
 
-    def money_earned(self):
-        return self.current_value - self.price_purchased
-
-    def __str__(self):
-        result = "House Info:"
-        result += " Year Built: " + str(self.year_built)
-        result += " Location: " + self.location
-        result += " Price Purchased: $" + str(round(self.price_purchased, 2))
-        result += " Current Value: $" + str(round(self.current_value, 2))
-        return result
-
+def display_house_info(year_built, location, price_purchased, current_value):
+    print("-------------------")
+    print("House Information:")
+    print("Year Built:", year_built)
+    print("Location:", location)
+    print("Price Purchased: $" + str(round(price_purchased, 2)))
+    print("Current Value: $" + str(round(current_value, 2)))
 
 def main():
-    year_built = int(input("Year built: "))
-    price = float(input("Purchase price: "))
-    current_year = int(input("Current year: "))
-    location = input("Location: ")
+    while True:
+        year_built = int(input("Year built: "))
+        price_purchased = float(input("Price Purchased: "))
+        current_year = int(input("Current year: "))
+        location = input("Location: ")
 
-    if current_year < year_built:
-        print("Error: Current year can't be before the house was built.")
-        return
+        if current_year < year_built:
+            print("Invalid input, Current year can't be before the house was built.")
+        else:
+            current_value = calculate_current_value(year_built, price_purchased, current_year)
+            display_house_info(year_built, location, price_purchased, current_value)
+            profit = money_earned(current_value, price_purchased)
+            print("Profit Earned: $" + str(round(profit, 2)))
+            print("-------------------")
 
-    house = House(year_built, price, location)
-    house.calculate_current_value(current_year)
-
-    print(house)
-    print("Profit Earned: $" + str(round(house.money_earned(), 2)))
-
+        again = input("Would you like to enter another house? (yes/no): ").strip().lower()
+        if again != 'yes':
+            print("FareWell!")
+            break
 
 main()
